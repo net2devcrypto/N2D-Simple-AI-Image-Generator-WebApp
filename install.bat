6a64199603d886f87e6525dc8b35666f514d9989
net2devcrypto <info@a3b.io>
2023-04-09 00:35:03 -0700

    update installer

---
+++ install.bat
@@ -0,0 +1,7 @@
+echo Net2dev AI Image Generator Installer
+
+call npm install
+cd backend\
+call npm i
+cls
+pause
---
+++ startapp.bat
@@ -0,0 +1,6 @@
+echo Net2dev AI Image Generator Executer
+
+start cmd /k npm run dev
+cd backend\
+start cmd /k node backend.js
+
